632279d7f744b8409e748c9964be18434e2ccf7a
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-14 14:12:40 +0200

    Add profile and configure signals

--- a_users/apps.py
+++ a_users/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AUsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'a_users'
+
+
+    def ready(self):
+        import a_users.signals
---
+++ a_users/signals.py
@@ -0,0 +1,12 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def user_postsave(sender, instance, created, **kwargs):
+    user = instance
+
+    if created:
+        Profile.objects.create(user=user,)
