3d59aa7bd1c21d81664217a5a28c22f4d518795f
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-14 16:04:36 +0200

    lower username

--- a_users/signals.py
+++ a_users/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.contrib.auth.models import User
 from .models import Profile
 
@@ -10,3 +10,9 @@ def user_postsave(sender, instance, created, **kwargs):
 
     if created:
         Profile.objects.create(user=user,)
+
+
+@receiver(pre_save, sender=User)
+def user_presave(sender, instance, **kwargs):
+    if instance.username:
+        instance.username = instance.username.lower()
