dfa7b5f516ff7401545f88b3ce5b53c37db74721
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-14 14:04:33 +0200

    Add new app

--- a_core/settings.py
+++ a_core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'a_home'
+    'a_home',
+    'a_users',
 ]
 
 MIDDLEWARE = [
