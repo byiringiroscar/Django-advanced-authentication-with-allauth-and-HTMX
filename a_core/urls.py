a2ceae55c49e0c0035a107a7a9c2d270c5cb7022
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-14 14:21:46 +0200

    Add profile page

--- a_core/urls.py
+++ a_core/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from a_home.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
+    path('profile/', include('a_users.urls')),
 ]
--- a_users/views.py
+++ a_users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def profile_view(request):
+    profile  = request.user.profile
+    context = {'profile': profile}
+    return render(request, 'a_users/profile.html', context)
