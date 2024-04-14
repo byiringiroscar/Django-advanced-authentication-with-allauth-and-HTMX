fcc1e5f7d6b6bab5267dcceaf985d79ff38fbabd
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-14 15:14:37 +0200

    Add profile edit

--- a_core/urls.py
+++ a_core/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from a_home.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('profile/', include('a_users.urls')),
-]
+] 
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- a_users/views.py
+++ a_users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ProfileForm
 from django.contrib.auth.decorators import login_required
 
@@ -14,4 +14,9 @@ def profile_view(request):
 @login_required
 def profile_edit_view(request):
     form = ProfileForm(instance=request.user.profile)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
     return render(request, 'a_users/profile_edit.html', {'form': form})
